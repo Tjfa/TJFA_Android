96914a79e47ec0567bd729afbfbca3baf1675339
VioletHill <qfviolethill@163.com>
2015-02-15 22:24:32 +0800

    修复错误

--- app/src/main/java/me/qiufeng/www/Activity/MainActivity.java
+++ app/src/main/java/me/qiufeng/www/Activity/MainActivity.java
@@ -3,21 +3,13 @@ package me.qiufeng.www.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import me.qiufeng.www.LogicalLayer.DataModule.DataManager.DatabaseManager;
-import me.qiufeng.www.LogicalLayer.DataModule.DataManager.FinishCallBack;
-import me.qiufeng.www.LogicalLayer.DataModule.DataManager.NewsManager;
-import me.qiufeng.www.LogicalLayer.DataModule.LocalModule.News;
 import me.qiufeng.www.R;
-import me.tangke.slidemenu.SlideMenu;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -26,7 +18,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        DatabaseManager.sharedDatabaseManager().deleteAll();
+        DatabaseManager.sharedDatabaseManager().clearAllTable();
         setContentView(R.layout.activity_main);
 
         Button newsButton = (Button)findViewById(R.id.news_button);
